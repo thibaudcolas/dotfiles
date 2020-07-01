9723dd5dc1e9d361905797b6534cad8ccf953a3e
Thibaud Colas <thibaudcolas@gmail.com>
2020-07-01 11:19:41 +0100

    Update VS Code extensions

--- config/visual-studio-code/install.sh
+++ config/visual-studio-code/install.sh
@@ -12,54 +12,42 @@ if [[ $? -eq 0 ]];then
         ok "Installing extensions please wait..."
 
         # Update this with code --list-extensions
-        code --install-extension abusaidm.html-snippets
-        code --install-extension andys8.jest-snippets
-        code --install-extension asvetliakov.snapshot-tools
-        code --install-extension batisteo.vscode-django
         code --install-extension bibhasdn.unique-lines
-        code --install-extension bpruitt-goddard.mermaid-markdown-syntax-highlighting
+        code --install-extension bierner.lit-html
         code --install-extension bungcip.better-toml
         code --install-extension christian-kohler.npm-intellisense
         code --install-extension christian-kohler.path-intellisense
         code --install-extension cssho.vscode-svgviewer
         code --install-extension DavidAnson.vscode-markdownlint
         code --install-extension dbaeumer.vscode-eslint
-        code --install-extension donjayamanne.githistory
-        code --install-extension dt.ghlink
+        code --install-extension ecmel.vscode-html-css
         code --install-extension EditorConfig.EditorConfig
-        code --install-extension eg2.vscode-npm-script
         code --install-extension esbenp.prettier-vscode
-        code --install-extension fb-pyre-check.pyre-vscode
         code --install-extension florianloch.text-transform
         code --install-extension flowtype.flow-for-vscode
-        code --install-extension freebroccolo.reasonml
-        code --install-extension glen-84.sass-lint
         code --install-extension hackwaly.ocaml
+        code --install-extension jaredly.reason-vscode
         code --install-extension jkjustjoshing.vscode-text-pastry
-        code --install-extension joelday.docthis
         code --install-extension jpoissonnier.vscode-styled-components
         code --install-extension lextudio.restructuredtext
         code --install-extension mgmcdermott.vscode-language-babel
-        code --install-extension mkaufman.HTMLHint
-        code --install-extension mohsen1.prettify-json
         code --install-extension monokai.theme-monokai-pro-vscode
         code --install-extension mrmlnc.vscode-json5
         code --install-extension mrmlnc.vscode-scss
+        code --install-extension mrorz.language-gettext
         code --install-extension ms-azuretools.vscode-docker
         code --install-extension ms-python.python
         code --install-extension ms-vscode.Theme-MarkdownKit
+        code --install-extension NathHorrigan.code-svgr
         code --install-extension OfHumanBondage.react-proptypes-intellisense
         code --install-extension PKief.material-icon-theme
         code --install-extension rust-lang.rust
-        code --install-extension sbrink.elm
-        code --install-extension shinnn.stylelint
+        code --install-extension silvenon.mdx
+        code --install-extension stylelint.vscode-stylelint
         code --install-extension sysoev.vscode-open-in-github
+        code --install-extension tlent.jest-snapshot-language-support
         code --install-extension VisualStudioExptTeam.vscodeintellicode
-        code --install-extension vstirbu.vscode-mermaid-preview
         code --install-extension whatwedo.twig
-        code --install-extension wix.glean
-        code --install-extension Zignd.html-css-class-completion
-        code --install-extension ziyasal.vscode-open-in-github
 
         ok "Extensions for VSC have been installed. Please restart your VSC."
     else
